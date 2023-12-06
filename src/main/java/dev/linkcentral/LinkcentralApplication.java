cb996bef2ef28000e34231e1ac8fc8b59dbd4884
minseok <alstjr706@naver.com>
2023-12-07 03:23:18 +0900

    Initial commit

---
+++ src/main/java/dev/linkcentral/LinkcentralApplication.java
@@ -0,0 +1,13 @@
+package dev.linkcentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkcentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkcentralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/linkcentral/LinkcentralApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.linkcentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkcentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
