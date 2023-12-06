aa1c63c677316208cede8ec2870d7635ea49e4ff
minseok <alstjr706@naver.com>
2023-12-07 04:12:28 +0900

    feat : Member Entity 구현

--- src/main/java/dev/linkcentral/database/entity/Member.java
+++ src/main/java/dev/linkcentral/database/entity/Member.java
@@ -1,4 +1,36 @@
 package dev.linkcentral.database.entity;
 
-public class Member {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@ToString(of = {"id", "name", "password", "email", "nickname"})
+@Entity
+public class Member extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Column(length = 50, unique = true)
+    private String name;
+
+    @Setter
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @Setter
+    @Column(length = 100, nullable = false)
+    private String email;
+
+    @Setter
+    @Column(length = 100, nullable = false)
+    private String nickname;
+
+    protected Member() {
+    }
 }
