ab808df4adb78c417c2790861e56e33245314b4a
minseok <alstjr706@naver.com>
2023-12-17 16:05:17 +0900

    feat : 좋아요 Entity 구현

---
+++ src/main/java/dev/linkcentral/database/entity/ArticleLikes.java
@@ -0,0 +1,23 @@
+package dev.linkcentral.database.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+public class ArticleLikes extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member memberId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id")
+    private Article articleId;
+
+}
---
+++ src/main/java/dev/linkcentral/database/entity/MemberFeedLikes.java
@@ -0,0 +1,23 @@
+package dev.linkcentral.database.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+public class MemberFeedLikes extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member memberId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_feed_id")
+    private MemberFeed memberFeedId;
+
+}
