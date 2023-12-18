95d653500472ad742b4638f71be8857e5f24ea00
minseok <alstjr706@naver.com>
2023-12-18 12:54:35 +0900

    refactor : DB 컬럼의 제약조건에 맞게 DTO 추가 수정

--- src/main/java/dev/linkcentral/service/dto/MemberSaveRequestDTO.java
+++ src/main/java/dev/linkcentral/service/dto/MemberSaveRequestDTO.java
@@ -12,18 +12,21 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 public class MemberSaveRequestDTO {
 
+    @Range(max = 50, message = "이름은 50자 이내로 입력해 주세요.")
     @NotBlank(message = "이름을 입력해주세요.")
     private String name;
 
+    @Range(max = 100, message = "비밀번호는 100자 이내로 입력해 주세요.")
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 
+    @Range(max = 100, message = "이메일은 100자 이내로 입력해 주세요.")
     @NotBlank(message = "이메일을 입력해주세요.")
     @Email(message = "올바른 이메일 주소를 입력해주세요.")
     private String email;
 
     @NotBlank(message = "닉네임을 입력해주세요.")
-    @Range(min = 2, max = 15, message = "닉네임은 2 ~ 15자 사이로 입력해주세요.")
+    @Range(max = 100, message = "닉네임은 100자 이내로 입력해 주세요.")
     private String nickname;
 
     private String role;
