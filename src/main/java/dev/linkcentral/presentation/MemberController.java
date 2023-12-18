ad0d03b254973b3096132a6adb55299821417073
minseok <alstjr706@naver.com>
2023-12-19 00:35:10 +0900

    feat : 로그인 API 구현

--- src/main/java/dev/linkcentral/presentation/MemberController.java
+++ src/main/java/dev/linkcentral/presentation/MemberController.java
@@ -1,13 +1,18 @@
 package dev.linkcentral.presentation;
 
+import dev.linkcentral.database.entity.Member;
 import dev.linkcentral.service.MemberService;
+import dev.linkcentral.service.dto.MemberLoginRequestDTO;
 import dev.linkcentral.service.dto.MemberSaveRequestDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,7 +27,7 @@ public class MemberController {
     }
 
     @GetMapping("/members/join-form")
-    public String createMemberForm() {
+    public String joinMember() {
         return "/members/join";
     }
 
@@ -31,4 +36,17 @@ public class MemberController {
         Long memberId = memberService.joinMember(memberSaveRequestDTO);
         return "/home";
     }
+
+    @PostMapping("/login")
+    public String login(MemberLoginRequestDTO MemberLoginRequestDTO, Model model) {
+        Optional<Member> member = memberService.loginMember(MemberLoginRequestDTO.getName(), MemberLoginRequestDTO.getPassword());
+
+        if (member.isEmpty()) {
+            model.addAttribute("loginMessage", "아이디 혹은 비밀번호가 일치하지 않습니다.");
+            return "/home";
+        }
+        member.map(Member::getName).ifPresent(memberName -> model.addAttribute("memberName", memberName));
+        return "/members/login";
+    }
+
 }
