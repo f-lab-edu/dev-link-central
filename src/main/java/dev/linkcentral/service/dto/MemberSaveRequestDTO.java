20b8bb28e781b6ab31bddee20cdd590db9628c88
minseok <alstjr706@naver.com>
2023-12-19 15:41:06 +0900

    refactor : DB 컬럼의 제약조건에 맞게 validation 작성

--- src/main/java/dev/linkcentral/service/dto/MemberSaveRequestDTO.java
+++ src/main/java/dev/linkcentral/service/dto/MemberSaveRequestDTO.java
@@ -1,6 +1,10 @@
 package dev.linkcentral.service.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.Email;
@@ -12,7 +16,7 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 public class MemberSaveRequestDTO {
 
-    @Range(max = 50, message = "이름은 50자 이내로 입력해 주세요.")
+    @Length(max = 50, message = "이름은 50자 이내로 입력해 주세요.")
     @NotBlank(message = "이름을 입력해주세요.")
     private String name;
 
@@ -20,13 +24,13 @@ public class MemberSaveRequestDTO {
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 
-    @Range(max = 100, message = "이메일은 100자 이내로 입력해 주세요.")
+    @Length(max = 100, message = "이메일은 100자 이내로 입력해 주세요.")
     @NotBlank(message = "이메일을 입력해주세요.")
     @Email(message = "올바른 이메일 주소를 입력해주세요.")
     private String email;
 
+    @Length(max = 100, message = "닉네임은 100자 이내로 입력해 주세요.")
     @NotBlank(message = "닉네임을 입력해주세요.")
-    @Range(max = 100, message = "닉네임은 100자 이내로 입력해 주세요.")
     private String nickname;
 
     private String role;
@@ -34,5 +38,4 @@ public class MemberSaveRequestDTO {
     public void updateRole(String role) {
         this.role = role;
     }
-
 }
