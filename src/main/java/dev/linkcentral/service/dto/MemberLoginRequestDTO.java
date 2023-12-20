c1197621a1c39bcdf9c3747422ed3dc07a3b06e9
minseok <alstjr706@naver.com>
2023-12-20 12:58:41 +0900

    feat : 로그인 하기 위한 id, pwd 받는 DTO 구현

---
+++ src/main/java/dev/linkcentral/service/dto/MemberLoginRequestDTO.java
@@ -0,0 +1,16 @@
+package dev.linkcentral.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberLoginRequestDTO {
+
+    private String name;
+    private String password;
+}
