83ee500c7098c21ef3c7420d5c0e6a1bd16d5121
minseok <alstjr706@naver.com>
2023-12-20 12:57:40 +0900

    git : sing-up 브랜치 rebase

--- src/main/java/dev/linkcentral/presentation/MemberController.java
+++ src/main/java/dev/linkcentral/presentation/MemberController.java
@@ -1,15 +1,16 @@
 package dev.linkcentral.presentation;
 
+import dev.linkcentral.database.entity.Member;
 import dev.linkcentral.service.MemberService;
+import dev.linkcentral.service.dto.MemberLoginRequestDTO;
 import dev.linkcentral.service.dto.MemberSaveRequestDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
 
 @Slf4j
 @RequiredArgsConstructor
@@ -36,4 +37,17 @@ public class MemberController {
 
         return "/home";
     }
+
+    @PostMapping("/login")
+    public String login(MemberLoginRequestDTO MemberLoginRequestDTO, Model model) {
+        Optional<Member> member = memberService.loginMember(MemberLoginRequestDTO.getName(), MemberLoginRequestDTO.getPassword());
+
+        if (member.isEmpty()) {
+            model.addAttribute("loginMessage", "아이디 혹은 비밀번호가 일치하지 않습니다.");
+            return "/home";
+        }
+        member.map(Member::getName).ifPresent(memberName -> model.addAttribute("memberName", memberName));
+        return "/members/login";
+    }
+
 }
