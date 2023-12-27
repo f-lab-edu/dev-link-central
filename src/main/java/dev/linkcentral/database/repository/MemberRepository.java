e203c066c93fa36e8a462d1731918f79f03b46f5
minseok <alstjr706@naver.com>
2023-12-27 11:16:44 +0900

    feat : 임시 비밀번호로 유저 비밀번호 변경 로직 구현

--- src/main/java/dev/linkcentral/database/repository/MemberRepository.java
+++ src/main/java/dev/linkcentral/database/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package dev.linkcentral.database.repository;
 
 import dev.linkcentral.database.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -9,8 +10,14 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    @Query("select m from Member m where m.name=:name and m.password=:password")
+    @Query("select m from Member m where m.name = :name and m.password = :password")
     Optional<Member> selectMember(@Param("name") String name, @Param("password") String password);
 
     boolean existsByNickname(String nickname);
+
+    Optional<Member> findByEmail(String memberEmail);
+
+    @Modifying
+    @Query("update Member m set m.password = :password where m.id = :id")
+    void updatePasswordById(@Param("id") Long id, @Param("password") String password);
 }
