830339541ffc909d3912dca9fb18f4ccacad47ba
minseok <alstjr706@naver.com>
2023-12-29 07:50:27 +0900

    refactor : 로그인 관련 DATA JPA 메서드 수정

--- src/main/java/dev/linkcentral/database/repository/MemberRepository.java
+++ src/main/java/dev/linkcentral/database/repository/MemberRepository.java
@@ -2,14 +2,10 @@ package dev.linkcentral.database.repository;
 
 import dev.linkcentral.database.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    @Query("select m from Member m where m.name=:name and m.password=:password")
-    Optional<Member> selectMember(@Param("name") String name, @Param("password") String password);
-
+    List<Member> findByName(String name);
 }
