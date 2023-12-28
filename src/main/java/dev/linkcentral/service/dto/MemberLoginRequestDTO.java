11683e58af457abf5a6e2667df1e15d976228a03
minseok <alstjr706@naver.com>
2023-12-29 07:49:08 +0900

    refactor : DTO 어노테이션 @Data 수정

--- src/main/java/dev/linkcentral/service/dto/MemberLoginRequestDTO.java
+++ src/main/java/dev/linkcentral/service/dto/MemberLoginRequestDTO.java
@@ -1,12 +1,8 @@
 package dev.linkcentral.service.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Setter
-@Getter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberLoginRequestDTO {
