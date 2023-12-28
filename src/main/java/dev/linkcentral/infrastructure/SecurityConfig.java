a07a82951105aa52f733d3d9567ce42e0e3bfbdf
minseok <alstjr706@naver.com>
2023-12-28 20:26:46 +0900

    feat : 시큐리티 빈 등록

---
+++ src/main/java/dev/linkcentral/infrastructure/SecurityConfig.java
@@ -0,0 +1,26 @@
+package dev.linkcentral.infrastructure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable();
+
+        return http.build();
+    }
+}
