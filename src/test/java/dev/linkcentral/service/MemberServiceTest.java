98ff404459f5839a1fee27d2971ff20b8d939025
minseok <alstjr706@naver.com>
2024-01-10 18:39:14 +0900

    [feature/test-code-member] build : Mockito 임포트

---
+++ src/test/java/dev/linkcentral/service/MemberServiceTest.java
@@ -0,0 +1,52 @@
+package dev.linkcentral.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    @Test
+    void joinMember() {
+    }
+
+    @Test
+    void loginMember() {
+    }
+
+    @Test
+    void isNicknameDuplicated() {
+    }
+
+    @Test
+    void userEmailCheck() {
+    }
+
+    @Test
+    void createMailAndChangePassword() {
+    }
+
+    @Test
+    void updatePassword() {
+    }
+
+    @Test
+    void generateTempPassword() {
+    }
+
+    @Test
+    void mailSend() {
+    }
+
+    @Test
+    void updateMember() {
+    }
+
+    @Test
+    void checkPassword() {
+    }
+
+    @Test
+    void deleteMember() {
+    }
+}
