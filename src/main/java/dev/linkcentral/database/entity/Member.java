978829ee3a421978380c42a8bdf5f1d5714bdfde
minseok <alstjr706@naver.com>
2024-01-12 20:04:35 +0900

    [feature/user-feature] refactor : 필요없는 임포트 제거.

--- src/main/java/dev/linkcentral/database/entity/Member.java
+++ src/main/java/dev/linkcentral/database/entity/Member.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
-import org.hibernate.annotations.SQLDelete;
 
 import javax.persistence.*;
 import java.io.Serializable;
