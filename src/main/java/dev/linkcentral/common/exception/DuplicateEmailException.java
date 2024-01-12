163e06df8bb84bedd7ed7c0c893f70407743e4c8
minseok <alstjr706@naver.com>
2024-01-12 20:04:01 +0900

    [feature/user-feature] docs : 주석 작성

--- src/main/java/dev/linkcentral/common/exception/DuplicateEmailException.java
+++ src/main/java/dev/linkcentral/common/exception/DuplicateEmailException.java
@@ -1,5 +1,8 @@
 package dev.linkcentral.common.exception;
 
+/**
+ * 중복된 이메일 예외.
+ */
 public class DuplicateEmailException extends RuntimeException {
     public DuplicateEmailException(String message) {
         super(message);
