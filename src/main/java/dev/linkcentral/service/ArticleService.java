4299a1a71bd7d34f2b470c22bf6c7f6d707adae8
minseok <alstjr706@naver.com>
2024-01-18 04:59:02 +0900

    [feature/article-pagination] feat : 게시판 페이징 처리 Offset 방법 로직 구현

--- src/main/java/dev/linkcentral/service/ArticleService.java
+++ src/main/java/dev/linkcentral/service/ArticleService.java
@@ -6,6 +6,10 @@ import dev.linkcentral.service.dto.request.ArticleRequestDTO;
 import dev.linkcentral.service.dto.request.ArticleUpdateRequestDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -53,5 +57,20 @@ public class ArticleService {
 
     public void delete(Long id) {
         articleRepository.deleteById(id);
+    }
+
+    public Page<ArticleRequestDTO> paging(Pageable pageable) {
+        int page = pageable.getPageNumber() - 1; // page 위치에 있는 값은 0부터 시작한다.
+        int pageLimit = 3; // 한 페이지에 보여줄 글 갯수
+
+        // 한페이지당 3개씩 글을 보여주고 정렬 기준은 id 기준으로 내림차순 정렬
+        Page<Article> articleEntity = articleRepository.findAll(PageRequest
+                .of(page, pageLimit, Sort.by(Sort.Direction.DESC, "id")));
+
+        return articleEntity.map(article -> new ArticleRequestDTO(
+                article.getId(),
+                article.getWriter(),
+                article.getTitle(),
+                article.getCreatedAt()));
     }
 }
