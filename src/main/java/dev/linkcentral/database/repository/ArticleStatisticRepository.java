119e7cd7cdac70339d53fbe7bc1168e99aaaba4a
minseok <alstjr706@naver.com>
2024-01-22 10:20:05 +0900

    [feature/article-like] feat : ArticleLike 레포지토리 구현

--- src/main/java/dev/linkcentral/database/repository/ArticleStatisticRepository.java
+++ src/main/java/dev/linkcentral/database/repository/ArticleStatisticRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ArticleStatisticRepository extends JpaRepository<ArticleStatistic, Long> {
+
     Optional<ArticleStatistic> findByArticle(Article article);
 }
