d191267db425dfb07e3da60e4119307daa0a2ee4
minseok <alstjr706@naver.com>
2024-01-30 18:09:52 +0900

    [feature/#68-profile-s3-storage] feat : AWS S3 Config 구현 - 액세스 키와 시크릿 키를 이용하여 AWS 인증 정보 생성

---
+++ src/main/java/dev/linkcentral/infrastructure/AwsS3Config.java
@@ -0,0 +1,34 @@
+package dev.linkcentral.infrastructure;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsS3Config {
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        // 액세스 키와 시크릿 키를 이용하여 AWS 기본 인증 정보를 생성
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        // AmazonS3Client 빌더를 사용하여 S3 클라이언트를 생성
+        // 설정된 리전과 인증 정보를 사용하여 클라이언트를 구성한다.
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
