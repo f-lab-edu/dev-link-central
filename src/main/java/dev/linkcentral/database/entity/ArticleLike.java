dd3d864d45a236a3d84c747c4ef3889476e92b70
minseok <alstjr706@naver.com>
2024-02-04 03:40:03 +0900

    [feature/article-like] feat : 좋아요 증가, 감소 로직, 프론트단 좋아요 기능 구현

--- src/main/java/dev/linkcentral/database/entity/ArticleLike.java
+++ src/main/java/dev/linkcentral/database/entity/ArticleLike.java
@@ -3,6 +3,7 @@ package dev.linkcentral.database.entity;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Getter
 @Entity
@@ -19,5 +20,26 @@ public class ArticleLike extends AuditingFields {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "article_id")
     private Article article;
+
+    protected ArticleLike() {}
+
+    public ArticleLike(Member member, Article article) {
+        this.member = member;
+        this.article = article;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleLike)) return false;
+        ArticleLike that = (ArticleLike) o;
+        return Objects.equals(member.getId(), that.member.getId()) &&
+                Objects.equals(article.getId(), that.article.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(member.getId(), article.getId());
+    }
 
 }
