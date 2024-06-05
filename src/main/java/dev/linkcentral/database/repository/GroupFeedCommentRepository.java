7b109427cc6e9fbf38da8cf290ed1b1263260a6f
Minseok Kim <alstjr706@naver.com>
2024-06-05 18:22:03 +0900

    [#98] feat : 피드 댓글 수정, 삭제 비즈니스 로직 구현

--- src/main/java/dev/linkcentral/database/repository/GroupFeedCommentRepository.java
+++ src/main/java/dev/linkcentral/database/repository/GroupFeedCommentRepository.java
@@ -4,8 +4,11 @@ import dev.linkcentral.database.entity.GroupFeedComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GroupFeedCommentRepository extends JpaRepository<GroupFeedComment, Long> {
 
     List<GroupFeedComment> findByGroupFeedId(Long groupFeedId);
+
+    Optional<GroupFeedComment> findByIdAndGroupFeedIdAndMemberId(Long id, Long groupFeedId, Long memberId);
 }
